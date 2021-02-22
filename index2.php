f32a5014fa7361a2c90f35ce9e56cc02efebb976
Beata Rynkevič <beata.rynkevic1@gmail.com>
2021-02-22 22:54:42 +0200

    4-6

--- index2.php
+++ index2.php
@@ -3,56 +3,63 @@
 Argumentą užrašykite taip, kad būtų galima įvesti tik sveiką skaičių; -->
 
 <?php
+echo '<pre>';
+function arDalijasi(int $skaicius) {
+    $dividers = 0;
+    for($i = 2; $i < $skaicius; $i++) {
+        if($skaicius % $i === 0) {
+            $dividers++;
+        }
+    }
+    return $dividers;
+}
 
-
-
-
+echo arDalijasi(11);
+// // foreach($data as $items) {
+// //     echo $items;
+// // }
+// print_r($data);
 ?>
 
 <h2>*******5*******</h2>
+<!-- Sugeneruokite masyvą iš 100 elementų, kurio reikšmės atsitiktiniai skaičiai nuo 33 iki 77.
+Išrūšiuokite masyvą pagal daliklių be liekanos kiekį mažėjimo tvarka, panaudodami ketvirto uždavinio funkciją. -->
+
 <?php
+$masyvas5 = [];
+foreach(range(1, 3) as $index => $value) {
+    $masyvas5[$index] = rand(2, 44);
+}
+print_r($masyvas5);
 
+function sortinimas($a, $b) {
+    return arDalijasi($b) <=> arDalijasi($a);
+}
 
+usort($masyvas5, "sortinimas");
+print_r($masyvas5);
 
-
+foreach($masyvas5 as $index => $value) {
+    echo "Skaicius $value dalijasi is: ". arDalijasi($value) . ' sveikuju skaiciu; <br>';
+}
 ?>
 
 <h2>*******6*******</h2>
+<!-- Sugeneruokite masyvą iš 100 elementų, kurio reikšmės atsitiktiniai skaičiai nuo 333 iki 777.
+Naudodami 4 uždavinio funkciją iš masyvo ištrinkite pirminius skaičius. -->
+
 <?php
+$masyvas6 = [];
+foreach(range(1, 10) as $index => $value) {
+    $masyvas6[] = rand(333, 777);
+}
+print_r($masyvas6);
+$ilgis = count($masyvas6); //i kintamaji, nes jeigu cikle, tai su kiekvienu ciklu masyvas trumpeja
 
-
-
-
-?>
-
-<h2>*******7*******</h2>
-<?php
-
-
-
-
-?>
-
-<h2>*******8*******</h2>
-<?php
-
-
-
-
-?>
-
-<h2>*******9*******</h2>
-<?php
-
-
-
-
-?>
-
-<h2>*******10*******</h2>
-<?php
-
-
-
-
+for($i = 0; $i < $ilgis; $i++) {
+    if(arDalijasi($masyvas6[$i]) === 0) { //jeigu 0 sveikuju skaiciu
+        unset($masyvas6[$i]);
+    }
+}
+print_r($masyvas6);
 ?>
---
+++ index3.php
@@ -0,0 +1,44 @@
+<h2>*******7*******</h2>
+<!-- Sugeneruokite atsitiktinio (nuo 10 iki 20) ilgio masyvą, kurio visi, išskyrus paskutinį, elementai yra atsitiktiniai skaičiai nuo 0 iki 10,
+o paskutinis masyvas, kuris generuojamas pagal tokią pat salygą kaip ir pirmasis masyvas. Viską pakartokite atsitiktinį nuo 10 iki 30  kiekį kartų.
+Paskutinio masyvo paskutinis elementas yra lygus 0; -->
+
+<?php
+
+
+
+
+?>
+
+<h2>*******8*******</h2>
+<!-- Suskaičiuokite septinto uždavinio elementų, kurie nėra masyvai, sumą. -->
+<?php
+
+
+
+
+?>
+
+<h2>*******9*******</h2>
+<!-- Sugeneruokite masyvą iš trijų elementų, kurie yra atsitiktiniai skaičiai nuo 1 iki 33.
+Jeigu tarp trijų paskutinių elementų yra nors vienas ne pirminis skaičius, prie masyvo pridėkite dar vieną elementą- atsitiktinį skaičių nuo 1 iki 33.
+Vėl patikrinkite pradinę sąlygą ir jeigu reikia pridėkite dar vieną elementą. Kartokite, kol sąlyga nereikalaus pridėti elemento.  -->
+
+<?php
+
+
+
+
+?>
+
+<h2>*******10*******</h2>
+<!-- Sugeneruokite masyvą iš 10 elementų, kurie yra masyvai iš 10 elementų, kurie yra atsitiktiniai skaičiai nuo 1 iki 100.
+Jeigu tokio masyvo pirminių skaičių vidurkis mažesnis už 70, suraskite masyve mažiausią skaičių (nebūtinai pirminį) ir prie jo pridėkite 3.
+Vėl paskaičiuokite masyvo pirminių skaičių vidurkį ir jeigu mažesnis nei 70 viską kartokite.  -->
+
+<?php
+
+
+
+
+?>
